5970fcd818ad69c23f5b7d45a395b7310077b764
Serkan Dezcan <serkandezcan@gmail.com>
2024-09-18 22:07:41 +0300

    v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,7 +2,10 @@ public class Runner {
     public static void main(String[] args) {
 
 
-        System.out.println("version1");
+        System.out.println("version 1");
+        System.out.println("version 2");
+        System.out.println("version 2 nin devamı");
+
 
 
     }
